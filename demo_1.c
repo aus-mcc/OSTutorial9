0fcb1c3c1a092ff89d8cded0c988b07f3c3d7e50
Prasheel12 <prasheel.bhagalia@uoit.net>
2018-03-26 17:43:06 -0400

    q1-commented demo1.c

--- demo_1.c
+++ demo_1.c
@@ -2,7 +2,9 @@
 #include <omp.h>
 int main (int argc, char *argv[])
 {
+    //Will be printed everytime the program is compiled
     printf("This statement will always print.\n");
+    //This part of the program will only print if -fopenmp is used in compilation. Everything after the #endif will be printed everytime program is compiled.
     #ifdef _OPENMP
     printf("This statement will only print when compiled with \"-fopenmp\"\n");
     #endif
