e8857b4a471f369a14ead3a34515423bfd042ae1
Austin McCulloch <austinjames.mcculloch@uoit.net>
2018-03-26 23:19:59 -0400

    segmentation fault error

--- question5.c
+++ question5.c
@@ -3,11 +3,34 @@
 #include <math.h>
 #include <omp.h>
 
+#define n 100000000
+double dx = 1/(n+1);
+
+
 int main(int argc, char *argv[]){
+
+    FILE *fp;
+    fp = fopen("calculation.txt","2");
+
     int thrdCount = atoi(argv[1]);
     int thread_num;
+    double x,y;
 
     #ifdef _OPENMP
-    omp_set_num_threads(thrdCount)
+    omp_set_num_threads(thrdCount);
     #endif
+
+    #pragma omp parallel for private(x)
+    for(int i=0;i<=n;i++){
+        x = i*dx;
+        y = exp(x)*cos(x)*sin(x)*sqrt(5*x+6.0);
+        #pragma omp critical
+        {
+            #ifdef _OPENMP
+                if(i%1000000==0){
+                    fprintf(fp, "Calculation Number: %d, X = %f, Y = %f\n", i,x,y);
+                }
+            #endif
+        }
+    }
 }
